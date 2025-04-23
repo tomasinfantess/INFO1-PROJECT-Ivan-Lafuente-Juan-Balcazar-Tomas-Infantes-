1053253773da75e7ce542a39d19fdf26aff9a8a8
tomasinfantess <tomas.infantes@estudiantat.upc.edu>
2025-04-23 09:59:19 +0200

    Delete segment.py

--- segment.py
+++
@@ -1,6 +0,0 @@
-class Segment:
-    def __init__(self, name, origin, destination):
-        self.name = name
-        self.origin = origin
-        self.destination = destination
-        self.cost = ((origin.x - destination.x)**2 + (origin.y - destination.y)**2)**0.5
